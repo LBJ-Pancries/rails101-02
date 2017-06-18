ad821e0e3e5fe42742863684de20961e3e40b8cd
liubingjie <breeder1121@hotmail.com>
2017-06-18 16:26:34 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  has_many :group_relationships
+  has_many :memders, through: :group_relationships, source: :user
   belongs_to :user
   has_many :posts
   validates :title, presence: true
