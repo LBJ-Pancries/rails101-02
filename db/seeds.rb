a9bdc88a3eddaf1d7bee7da4cd0da8ef88c4ac65
liubingjie <breeder1121@hotmail.com>
2017-06-18 16:13:39 +0800

    add seeds.rd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '建立一个账号'
+u = User.new
+u.email = "admin@1.com"
+u.password = "abcabc"
+u.password_confirmation = "abcabc"
+u.save
+
+puts '建立四个groups'
+Group.create!(title: '天天听英语社群', description: '在这里每天都要听英语', user_id: '1')
+Group.create!(title: '天天说英语社群', description: '在这里每天都要说英语', user_id: '1')
+Group.create!(title: '天天读英语社群', description: '在这里每天都要读英语', user_id: '1')
+Group.create!(title: '天天写英语社群', description: '在这里每天都要写英语', user_id: '1')
